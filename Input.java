10589b419a0de0cf85b4087d3086696f1c7f1a9c
VARSHA DAWLAGHAR <varshadawlagar@gmail.com>
2026-04-28 20:35:41 +0530

    Update Input.java

--- Input.java
+++ Input.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Input {
     public static void main(String[] args) {
+        System.out.println("Enter a number:");
         Scanner sc = new Scanner(System.in);
         
         int a = sc.nextInt();
@@ -9,3 +10,5 @@ class Input {
         System.out.println("result " + a);
     }
 }
+//output : Enter a number: 5 # user gives the input
+        //result 5
