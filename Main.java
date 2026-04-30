28d460b50b8f587f575bcdae3da08d69bb07a686
VARSHA DAWLAGHAR <varshadawlagar@gmail.com>
2026-04-30 23:16:34 +0530

    Implement basic calculator in Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+
+        System.out.println("Enter operator (+, -, *, /):");
+        char op = sc.next().charAt(0);
+
+        switch(op) {
+            case '+':
+                System.out.println("Result: " + (a + b));
+                break;
+
+            case '-':
+                System.out.println("Result: " + (a - b));
+                break;
+
+            case '*':
+                System.out.println("Result: " + (a * b));
+                break;
+
+            case '/':
+                if(b != 0)
+                    System.out.println("Result: " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
